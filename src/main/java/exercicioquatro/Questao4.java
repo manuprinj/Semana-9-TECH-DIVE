71198abf30a1685c7a6c1c49ac8ad5fcc180d4f0
manuprinj <manuelaprinjacinto@gmail.com>
2022-02-13 18:14:54 -0300

    Ex 4
    Change-Id: I99f87681bdef9fdcf11ecf3fdc294c0a547fae68

---
+++ src/main/java/exercicioquatro/Questao4.java
@@ -0,0 +1,31 @@
+package exercicioquatro;
+
+
+import java.util.Scanner;
+
+
+public class Questao4 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Eu sei dividir");
+        System.out.println("Informe o primeiro valor");
+        double x = teclado.nextInt();
+        Integer y = null;
+
+        while (y == null) {
+            try {
+                System.out.println("Informe o segundo valor");
+                int input = teclado.nextInt();
+                if (input == 0) {
+                    throw new IllegalArgumentException("O valor não pode ser zero!");
+                }
+                y = input;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        double r = x/y;
+        System.out.println("Resultado da divisão: " + r);
+    }
+}
